cbe65b3ddffd89536cf3577c8ad2c933e9eff31e
Bidhan Rai <bidhanrai424@gmail.com>
2021-02-19 17:17:30 +0545

    first commit

---
+++ Hacker News/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Bidhan Rai on 19/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts){ post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    })
+            }
+            .navigationBarTitle("Hacker News")
+        }
+        .onAppear(perform: {
+            networkManager.fetchData()
+        })
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Hacker News
+//
+//  Created by Bidhan Rai on 19/02/2021.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
