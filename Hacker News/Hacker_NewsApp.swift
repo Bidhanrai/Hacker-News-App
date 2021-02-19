1550d48a72ced79c84844cd0860b1481186cb867
Bidhan Rai <bidhanrai424@gmail.com>
2021-02-19 15:48:43 +0545

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Bidhan Rai on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
